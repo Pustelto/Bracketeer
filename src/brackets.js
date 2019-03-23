992bfeea656dffdacf8bb1b487ab131e84c23911
Tomas Pustelnik <10606260+Pustelto@users.noreply.github.com>
2019-03-23 22:38:44 +0100

    feat: add support mechanism for different languages
    Language support is ensure using json config which allows us to define
    different config per language (as each language may use different set of
    quotes and brackets). Thanks to this is should be easy to add support
    for languages which are not supported out of the box.

---
+++ src/brackets.js
@@ -0,0 +1,100 @@
+const {
+  isBracketToken,
+} = require('./helpers')
+
+/* Method get opening bracket position START
+
+- params: tokenizedBeforeText, langDef
+- shared variables between open and close - bracketType
+*/
+function getBracketPairPositionsAndType(tokenizedBeforeText, tokenizedAfterText, languageDef) {
+  const { brackets } = languageDef
+  const openingBrackets = brackets.map(b => b[0])
+  const closingBrackets = brackets.map(b => b[1])
+
+  // Helper variables
+  // const ENDERS = {
+  //   ')': '(',
+  //   ']': '[',
+  //   '}': '{',
+  // }
+  const ENDERS = brackets.reduce((obj, b) => ({...obj, [b[1]]: b[0]}), {})
+
+
+  // Parse opening bracket
+  // const bracketsTracker = {
+    //   '(': [],
+    //   '[': [],
+    //   '{': [],
+    // }
+  const bracketsTracker = brackets.reduce((obj, b) => ({...obj, [b[0]]: []}), {})
+  let bracketType, openPos, closePos
+
+  let i = tokenizedBeforeText.length - 1;
+  let beforeOffset = 0;
+
+  while (i >= 0) {
+    const tokenContent = tokenizedBeforeText[i].content
+    const isBracket = isBracketToken(tokenizedBeforeText[i].type, tokenContent, languageDef)
+
+    beforeOffset += tokenizedBeforeText[i].length
+
+    if (isBracket) {
+      if (openingBrackets.includes(tokenContent) && !bracketsTracker[tokenContent].length ) {
+        openPos = beforeOffset
+        bracketType = tokenContent
+        break;
+      } else {
+        openingBrackets.includes(tokenContent)
+        ? bracketsTracker[tokenContent].pop()
+        : bracketsTracker[ENDERS[tokenContent]].push(tokenContent)
+      }
+    }
+
+    i--;
+  }
+
+  if (openPos === undefined) return [];
+  /* Method get opening bracket position END */
+
+  /* Method get closing bracket position START */
+
+  // Parse closing bracket
+  let j = 0;
+  let afterOffset = 0;
+  const pairs = []
+
+  // const B_PAIRS = {
+  //   '(': '()',
+  //   '[': '[]',
+  //   '{': '{}',
+  // }
+  const B_PAIRS = brackets.reduce((obj, b) => ({...obj, [b[0]]: b}), {})
+
+  while (j < tokenizedAfterText.length) {
+    const tokenContent = tokenizedAfterText[j].content
+    const isCorrectBracketType = tokenizedAfterText[j].type === 'punctuation' &&
+    B_PAIRS[bracketType].indexOf(tokenContent) >= 0
+
+    if (isCorrectBracketType) {
+      if (closingBrackets.includes(tokenContent) && !pairs.length ) {
+        closePos = afterOffset
+        break;
+      } else {
+        closingBrackets.includes(tokenContent)
+        ? pairs.pop()
+        : pairs.push(tokenContent)
+      }
+    }
+
+    afterOffset += tokenizedAfterText[j].length
+
+    j++;
+  }
+  /* Method get closing bracket position END */
+  if (closePos === undefined) return [];
+
+  return [openPos, closePos, bracketType]
+}
+
+exports.getBracketPairPositionsAndType = getBracketPairPositionsAndType
--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,7 @@
 const vscode = require('vscode');
 const Prism = require('prismjs');
 const languages = require('prism-languages');
+const langDefs = require('./language-definitions.json')
 
 /* Used in brackets and quote parsing */
 const getParseLanguage = langId => Prism.languages[langId] || Prism.languages['javascript']
@@ -12,21 +13,27 @@ const getRangePosition = (selection, pos) => {
     const editor = vscode.window.activeTextEditor;
 
     return editor.document.getWordRangeAtPosition(selection[pos])
-        ? editor.document.getWordRangeAtPosition(selection[pos])[pos]
-        : selection[pos]
+    ? editor.document.getWordRangeAtPosition(selection[pos])[pos]
+    : selection[pos]
 }
 
-function isBracketToken(tokenType, tokenContent) {
-    return tokenType === 'punctuation' && '()[]{}'.indexOf(tokenContent) >= 0
+function isBracketToken(tokenType, tokenContent, langDef) {
+    const { bracket_tokens, brackets } = langDef;
+
+    return bracket_tokens.includes(tokenType) && brackets.join('').indexOf(tokenContent) >= 0
 }
 
 /* Used for quotes parsing */
-const isStringToken = (token) => token.type === 'template-string' || token.type === 'string'
+function isStringToken (token, langDef) {
+    const { string_tokens } = langDef;
+
+    return string_tokens.includes(token.type)
+}
 
 const tokenAtCursorPos = (token, offset, startOffset, cursorOffset) => {
     // If startOffset + offset is same as cursor - quotes are selected as well and we shouldn't do a quotes swap
     return token.length + offset + startOffset >= cursorOffset &&
-        offset + startOffset !== cursorOffset
+    offset + startOffset !== cursorOffset
 }
 
 exports.getParseLanguage = getParseLanguage
@@ -38,69 +45,86 @@ exports.tokenAtCursorPos = tokenAtCursorPos
 
 /* Larger heleprs function */
 exports.contentSelection = function contentSelection(startPos, endPos, originalSelection) {
-  const {start, end} = originalSelection
-  const selStart = new vscode.Position(startPos.line, startPos.character + 1)
+    const {start, end} = originalSelection
+    const selStart = new vscode.Position(startPos.line, startPos.character + 1)
 
-  // If current selection is same as new created one we will expand ti include brackets/quotes as well
-  if ( start.isEqual(selStart) && end.isEqual(endPos) ) {
-      return new vscode.Selection(startPos, new vscode.Position(endPos.line, endPos.character + 1))
-  }
+    // If current selection is same as new created one we will expand ti include brackets/quotes as well
+    if ( start.isEqual(selStart) && end.isEqual(endPos) ) {
+        return new vscode.Selection(startPos, new vscode.Position(endPos.line, endPos.character + 1))
+    }
 
-  return new vscode.Selection(selStart, endPos)
+    return new vscode.Selection(selStart, endPos)
 }
 
-function replaceTokens(selections, target) {
-  vscode.window.activeTextEditor.edit(edit => {
-      selections.forEach(sel => {
-          const { startPos, endPos, tokenType } = sel
-          let o
-          let e
+function replaceTokens(selections, tokenFamily, target) {
+    if (!selections.length) return;
 
-          if (target !== undefined) {
-              if (target.length !== 2 || target.length !== 0 || typeof target !== 'string') {
-                  new Error ('Target must be string of length 2 for token replacment or empty string for token deletion')
-              }
+    const languageId = vscode.window.activeTextEditor.document.languageId;
+    const languageDef = langDefs[languageId] || langDefs.javascript;
+    var a = langDefs["javascript"]
 
-              if (target.length === 0) {
-                  o = ''
-                  e = ''
-              } else {
-                  o = target[0]
-                  e = target[1] || target[0]
-              }
-          } else {
-              // TODO: this must be done universaly in order to allow future extensions
-              if ('([{'.indexOf(tokenType) >= 0) {
-                  o = tokenType === '(' ? '[' : tokenType === '[' ? '{' : '('
-                  e = tokenType === '(' ? ']' : tokenType === '[' ? '}' : ')'
-              } else {
-                  o = tokenType === "'" ? '"' : tokenType === '"' ? '`' : "'"
-                  e = o
-              }
-          }
+    const tokens = languageDef[tokenFamily];
 
-          edit.replace(charRange(startPos), o)
-          edit.replace(charRange(endPos), e)
-      })
-  })
+    vscode.window.activeTextEditor.edit(edit => {
+        selections.forEach(sel => {
+            const { startPos, endPos, tokenType } = sel
+            let o
+            let e
+
+            // we specified by what the selections should be replaced
+            if (target !== undefined) {
+                if (target.length !== 2 || target.length !== 0 || typeof target !== 'string') {
+                    new Error ('Target must be string of length 2 for token replacment or empty string for token deletion')
+                }
+
+                if (target.length === 0) {
+                    o = ''
+                    e = ''
+                } else {
+                    o = target[0]
+                    e = target[1] || target[0]
+                }
+                //   default replace cycling
+            } else {
+                // TODO: this must be done universaly in order to allow future extensions
+                const openingTokens = tokens.map(token => token[0]);
+                const closingTokens = tokens.map(token => token.length === 2 ? token[1] : token[0]);
+                const numOfTokens = tokens.length;
+
+                // [ (), [], {}]
+                const startIndex = openingTokens.findIndex(oT => oT === tokenType)
+
+                if (startIndex === -1) {
+                    vscode.window.showInformationMessage('Invalid token was parsed. Please try again or try another selection, cursor position.')
+                    return;
+                }
+
+                o = openingTokens[(startIndex + 1) % numOfTokens]
+                e = closingTokens[(startIndex + 1) % numOfTokens]
+            }
+
+            edit.replace(charRange(startPos), o)
+            edit.replace(charRange(endPos), e)
+        })
+    })
 }
 
 exports.replaceTokens = replaceTokens
 
 // Shamelessly taken from [Quick and Simple Text Selection](https://marketplace.visualstudio.com/items?itemName=dbankier.vscode-quick-select) by David Bankier
 function charRange(p) {
-  let end_pos = new vscode.Position(p.line, p.character + 1);
-  return new vscode.Selection(p, end_pos)
+    let end_pos = new vscode.Position(p.line, p.character + 1);
+    return new vscode.Selection(p, end_pos)
 }
 
 exports.charRange = charRange
 
 function getPositionFromOffset(position, offset, before = true) {
-  const editor = vscode.window.activeTextEditor;
-  const d = editor.document
-  const delta = before ? offset : offset * -1
+    const editor = vscode.window.activeTextEditor;
+    const d = editor.document
+    const delta = before ? offset : offset * -1
 
-  return d.positionAt(d.offsetAt(position) - delta)
+    return d.positionAt(d.offsetAt(position) - delta)
 }
 
 exports.getPositionFromOffset = getPositionFromOffset;
