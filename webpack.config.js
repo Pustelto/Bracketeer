2019097a43f501d108c177ba262c719c5878728b
Tomas Pustelnik <10606260+Pustelto@users.noreply.github.com>
2020-10-25 12:13:21 +0100

    chore: setup automatic release

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const path = require("path");
+
+const config = {
+  target: "node",
+  entry: "./src/extension.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "extension.js",
+    libraryTarget: "commonjs2",
+    devtoolModuleFilenameTemplate: "../[resource-path]",
+  },
+  externals: {
+    vscode: "vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+  },
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+};
+
+if (process.env.NODE_ENVIRONMENT !== "production") {
+  config.devtool = "source-map";
+}
+module.exports = config;
